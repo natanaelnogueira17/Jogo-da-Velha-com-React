f7e48ab9012ee8abbc4675448ae8fef7d17f0d77
natanael nogueira <natanaelnogueira019@gmail.com>
2022-07-27 23:02:40 -0300

    criando o botao para reiniciar o jogo

--- pages/src/components/button/Button.jsx
+++ pages/src/components/button/Button.jsx
@@ -1,8 +1,8 @@
 import style from './Button.module.css'
 
-function Button ({children, onClick, disabled}){
+function Button ({children, onClick}){
   return(
-    <button  className={style.button} onClick = {onClick} disabled={disabled}>
+    <button  className={style.button} onClick = {onClick} >
     {children}
     </button>
   )
